e13dae7b728b7d3d6c2046643d2762c56aeb3ab0
simcli <simoncliff61@gmail.com>
2022-12-09 11:34:13 -0800

    Add project files.

---
+++ Lab5/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /*
+            Name: Yassine Adaam Ammantoola
+            Course: PROG1017: Introduction to Programming - Lab 5: Functions and Loops
+            
+         */
+        // Constant to display name
+        const string PROGRAMMER = "Yassine Adaam Ammantoola";
+        int loginAttempts1 = 0;
+
+        //Function to get random number from minimum to maximum
+        public static int GetRandom(int min, int max)
+        {
+            Random randomNum = new Random();
+            int number = randomNum.Next(min, max);
+            return number;
+        }
+
+        //On form load, generate random number and hide the groupboxes
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = this.Text + " " + PROGRAMMER;
+            grpChoose.Visible = false;
+            grpText.Visible = false;
+            grpStats.Visible = false;
+            txtCode.Focus();
+            lblCode.Text = GetRandom(100000, 200001).ToString();
+
+            //string text = form1.title + PROGRAMMER;
+        }
+        //On login button click, Check login details if it matches
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (lblCode.Text == txtCode.Text)
+            {
+                grpChoose.Visible = true;
+                grpLogin.Enabled = false;
+            }
+            else
+            {
+                loginAttempts++;
+                if (loginAttempts < 3)
+                {
+                    MessageBox.Show(loginAttempts + " Incorrect code(s) entered\n " + "Try again - only 3 attempts allowed", PROGRAMMER);
+                }
+                else
+                {
+                    MessageBox.Show("3 Attempts to login\n " + "Account locked - Closing program", PROGRAMMER);
+                    this.Close();
+
+                }
+            }
+        }
+
+        // function to reset textbox strings and  checkbox and lable results
+        public void ResetTextGrp()
+        {
+            txtString1.Text = "";
+            txtString2.Text = "";
+            chkSwap.Checked = false;
+            lblResults.Text = "";
+
+        }
+
+        //Reset Stats groupbox labels and listbox 
+        public void ResetStatsGrp()
+        {
+            lblSum.Text = "";
+            lblMean.Text = "";
+            lblOdd.Text = "";
+            lstNumbers.Items.Clear();
+        }
+        //function to check id radio button is clicked
+        public void SetupOption()
+        {
+            if (radText.Checked == true)
+            {
+                grpText.Visible = false;
+                grpStats.Visible = true;
+                ResetStatsGrp();
+            }
+            if (radStats.Checked == true)
+            {
+                grpStats.Visible = true;
+                grpText.Visible = false;
+                ResetStatsGrp();
+            }
+        }
+        // When text radio button is checked, call SetupOption()
+        private void radText_CheckedChanged(object sender, EventArgs e)
+        {
+            SetupOption();
+        }
+        // When stats radio button is checked, call SetupOption()
+        private void radStats_CheckedChanged(object sender, EventArgs e)
+        {
+            SetupOption();
+        }
+        // When button reset is clicked call ResetTextGrp()
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetTextGrp();
+        }
+        // When button clear is clicked call ResetStatsGrp()
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ResetStatsGrp();
+        }
+
+        // Function Swap that accepts to strings and swaps them
+        public void Swap(string string1, string string2)
+        {
+            string1 = string1 + string2;
+
+            string2 = string1.Substring(0, string1.Length - string2.Length);
+            string1 = string1.Substring(string2.Length);
+            txtString1.Text = string2;
+            txtString2.Text = string1;
+        }
+        //Function CheckInput to check if string1 and string2 is empty
+        public bool CheckInput()
+        {
+            bool testData = true;
+            if ((txtString1.Text == "" && txtString2.Text == ""))
+            {
+                testData = false;
+            }
+            return testData;
+
+
+        }
+
+        //When swap checkbox is clicked, interchange strings
+        private void chkSwap_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkSwap.Checked == true && !(txtString1.Text == "" && txtString2.Text == ""))
+            {
+                string String_1 = txtString1.Text;
+                string String_2 = txtString2.Text;
+
+                CheckInput();
+                Swap(String_2, String_1);
+
+                lblResults.Text = "Strings have been swapped!";
+
+            }
+        }
+
+        //On click of Join Button, Join the strings
+        private void btnJoin_Click(object sender, EventArgs e)
+        {
+
+            if (!(txtString1.Text == "" && txtString2.Text == ""))
+            {
+
+                lblResults.Text = "First String = " + txtString1.Text +
+                              "\nSecond String = " + txtString2.Text +
+                              "\nJoined = " + txtString1.Text + "-->" + txtString2.Text;
+            }
+        }
+        //On click of Analyze Button, Calculate the number of characters
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            if (!(txtString1.Text == "" && txtString2.Text == ""))
+            {
+
+                lblResults.Text = "First string = " + txtString1.Text +
+                                  "\n Characters = " + txtString1.Text.Length +    
+                              "\nSecond String = " + txtString2.Text +
+                               "\n Characters = " + txtString2.Text.Length;
+            }
+        }
+        /* On click of Generate Button, Generate random numbers between 1000 and 5000 
+          and display on appropriate labels
+         */
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            lstNumbers.Text = "";
+            
+            Random rnd = new Random(733);
+            for (int i = 1; i <= numericUpDown1.Value; i++)
+            {
+               
+                lstNumbers.Items.Add(rnd.Next(1000,5001).ToString());
+
+            }
+            AddList();
+            lblMean.Text = "";
+            double avg = 0, summation = 0;
+            int lstCount = lstNumbers.Items.Count; //Get Count of Listbox Items
+            for (int i = 0; i < lstCount; i++)
+            {
+                summation += Convert.ToDouble(lstNumbers.Items[i]);
+            }
+            avg = summation / lstCount;
+            lblMean.Text = String.Format("{0:n}", avg).ToString();
+
+            CountOdd();
+        }
+
+        //function AddList() to add the values in the listbox
+        public int AddList()
+        {
+            int i = 0;
+            int sum = 0;
+            while(i > lstNumbers.Items.Count)
+            {
+                sum += Convert.ToInt32(lstNumbers.Items[i]);
+                lblSum.Text = String.Format("{0:n0}", sum).ToString();
+                i++;
+            }
+            return sum;
+        }
+
+        //Function to count the odd numbers in the listbox
+        public int CountOdd()
+        {
+            int i = 0;
+            int odd = 0;
+            do
+            {
+                if(Convert.ToInt32(lstNumbers.Items[i]) % 2 != 0)
+                {
+                    odd++;
+                    lblOdd.Text = odd.ToString();
+                   
+                }
+                i++;
+            }
+            while (i < lstNumbers.Items.Count);
+       
+            return odd;
+        }
+    }
+}
+
+    
---
+++ Lab5/Lab5/Lab5/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpLogin = new System.Windows.Forms.GroupBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCode = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grpChoose = new System.Windows.Forms.GroupBox();
+            this.radStats = new System.Windows.Forms.RadioButton();
+            this.radText = new System.Windows.Forms.RadioButton();
+            this.grpText = new System.Windows.Forms.GroupBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.btnJoin = new System.Windows.Forms.Button();
+            this.lblResults = new System.Windows.Forms.Label();
+            this.chkSwap = new System.Windows.Forms.CheckBox();
+            this.txtString2 = new System.Windows.Forms.TextBox();
+            this.txtString1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.grpStats = new System.Windows.Forms.GroupBox();
+            this.grpLogin.SuspendLayout();
+            this.grpChoose.SuspendLayout();
+            this.grpText.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpLogin
+            // 
+            this.grpLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.grpLogin.Controls.Add(this.btnLogin);
+            this.grpLogin.Controls.Add(this.txtCode);
+            this.grpLogin.Controls.Add(this.label2);
+            this.grpLogin.Controls.Add(this.lblCode);
+            this.grpLogin.Controls.Add(this.label1);
+            this.grpLogin.Location = new System.Drawing.Point(18, 16);
+            this.grpLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.grpLogin.Name = "grpLogin";
+            this.grpLogin.Padding = new System.Windows.Forms.Padding(4);
+            this.grpLogin.Size = new System.Drawing.Size(719, 73);
+            this.grpLogin.TabIndex = 0;
+            this.grpLogin.TabStop = false;
+            this.grpLogin.Text = "Login";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(593, 22);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(102, 38);
+            this.btnLogin.TabIndex = 1;
+            this.btnLogin.Text = "&Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(404, 33);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(109, 23);
+            this.txtCode.TabIndex = 0;
+            this.txtCode.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(343, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Type:";
+            // 
+            // lblCode
+            // 
+            this.lblCode.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCode.Location = new System.Drawing.Point(207, 33);
+            this.lblCode.Name = "lblCode";
+            this.lblCode.Size = new System.Drawing.Size(98, 27);
+            this.lblCode.TabIndex = 1;
+            this.lblCode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Authentication Code:";
+            // 
+            // grpChoose
+            // 
+            this.grpChoose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.grpChoose.Controls.Add(this.radStats);
+            this.grpChoose.Controls.Add(this.radText);
+            this.grpChoose.Location = new System.Drawing.Point(18, 97);
+            this.grpChoose.Margin = new System.Windows.Forms.Padding(4);
+            this.grpChoose.Name = "grpChoose";
+            this.grpChoose.Padding = new System.Windows.Forms.Padding(4);
+            this.grpChoose.Size = new System.Drawing.Size(719, 73);
+            this.grpChoose.TabIndex = 1;
+            this.grpChoose.TabStop = false;
+            this.grpChoose.Text = "Choose...";
+            // 
+            // radStats
+            // 
+            this.radStats.AutoSize = true;
+            this.radStats.Location = new System.Drawing.Point(442, 28);
+            this.radStats.Name = "radStats";
+            this.radStats.Size = new System.Drawing.Size(60, 20);
+            this.radStats.TabIndex = 1;
+            this.radStats.TabStop = true;
+            this.radStats.Text = "Stats";
+            this.radStats.UseVisualStyleBackColor = true;
+            // 
+            // radText
+            // 
+            this.radText.AutoSize = true;
+            this.radText.Location = new System.Drawing.Point(82, 28);
+            this.radText.Name = "radText";
+            this.radText.Size = new System.Drawing.Size(54, 20);
+            this.radText.TabIndex = 0;
+            this.radText.TabStop = true;
+            this.radText.Text = "Text";
+            this.radText.UseVisualStyleBackColor = true;
+            // 
+            // grpText
+            // 
+            this.grpText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.grpText.Controls.Add(this.btnReset);
+            this.grpText.Controls.Add(this.btnAnalyze);
+            this.grpText.Controls.Add(this.btnJoin);
+            this.grpText.Controls.Add(this.lblResults);
+            this.grpText.Controls.Add(this.chkSwap);
+            this.grpText.Controls.Add(this.txtString2);
+            this.grpText.Controls.Add(this.txtString1);
+            this.grpText.Controls.Add(this.label4);
+            this.grpText.Controls.Add(this.label3);
+            this.grpText.Location = new System.Drawing.Point(18, 179);
+            this.grpText.Name = "grpText";
+            this.grpText.Size = new System.Drawing.Size(356, 238);
+            this.grpText.TabIndex = 2;
+            this.grpText.TabStop = false;
+            this.grpText.Text = "Text";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(261, 205);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "&Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(140, 205);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalyze.TabIndex = 4;
+            this.btnAnalyze.Text = "&Analyze";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            // 
+            // btnJoin
+            // 
+            this.btnJoin.Location = new System.Drawing.Point(19, 205);
+            this.btnJoin.Name = "btnJoin";
+            this.btnJoin.Size = new System.Drawing.Size(75, 23);
+            this.btnJoin.TabIndex = 3;
+            this.btnJoin.Text = "&Join";
+            this.btnJoin.UseVisualStyleBackColor = true;
+            // 
+            // lblResults
+            // 
+            this.lblResults.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblResults.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResults.Location = new System.Drawing.Point(19, 109);
+            this.lblResults.Name = "lblResults";
+            this.lblResults.Size = new System.Drawing.Size(317, 82);
+            this.lblResults.TabIndex = 5;
+            // 
+            // chkSwap
+            // 
+            this.chkSwap.AutoSize = true;
+            this.chkSwap.Location = new System.Drawing.Point(23, 86);
+            this.chkSwap.Name = "chkSwap";
+            this.chkSwap.Size = new System.Drawing.Size(63, 20);
+            this.chkSwap.TabIndex = 2;
+            this.chkSwap.Text = "Swap";
+            this.chkSwap.UseVisualStyleBackColor = true;
+            // 
+            // txtString2
+            // 
+            this.txtString2.Location = new System.Drawing.Point(124, 58);
+            this.txtString2.Name = "txtString2";
+            this.txtString2.Size = new System.Drawing.Size(212, 23);
+            this.txtString2.TabIndex = 1;
+            // 
+            // txtString1
+            // 
+            this.txtString1.Location = new System.Drawing.Point(124, 20);
+            this.txtString1.Name = "txtString1";
+            this.txtString1.Size = new System.Drawing.Size(212, 23);
+            this.txtString1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "String #2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "String #1:";
+            // 
+            // grpStats
+            // 
+            this.grpStats.BackColor = System.Drawing.Color.PeachPuff;
+            this.grpStats.Location = new System.Drawing.Point(381, 179);
+            this.grpStats.Name = "grpStats";
+            this.grpStats.Size = new System.Drawing.Size(356, 238);
+            this.grpStats.TabIndex = 3;
+            this.grpStats.TabStop = false;
+            this.grpStats.Text = "Stats";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnLogin;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 418);
+            this.Controls.Add(this.grpStats);
+            this.Controls.Add(this.grpText);
+            this.Controls.Add(this.grpChoose);
+            this.Controls.Add(this.grpLogin);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab5 by";
+            this.grpLogin.ResumeLayout(false);
+            this.grpLogin.PerformLayout();
+            this.grpChoose.ResumeLayout(false);
+            this.grpChoose.PerformLayout();
+            this.grpText.ResumeLayout(false);
+            this.grpText.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpLogin;
+        private System.Windows.Forms.Label lblCode;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox grpChoose;
+        private System.Windows.Forms.GroupBox grpText;
+        private System.Windows.Forms.GroupBox grpStats;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radStats;
+        private System.Windows.Forms.RadioButton radText;
+        private System.Windows.Forms.CheckBox chkSwap;
+        private System.Windows.Forms.TextBox txtString2;
+        private System.Windows.Forms.TextBox txtString1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.Button btnJoin;
+        private System.Windows.Forms.Label lblResults;
+    }
+}
+
